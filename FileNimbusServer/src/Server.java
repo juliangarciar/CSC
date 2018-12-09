2e965530f8a94265b9b5f68f87d954e93dbecb4f
themarioga <marioga2512@gmail.com>
2018-12-09 19:43:20 +0100

    Commit

--- FileNimbusServer/src/Server.java
+++ FileNimbusServer/src/Server.java
@@ -14,7 +14,7 @@ public class Server{
 	
 	private int client;
 	
-	public Server(int server_port, String server_ip) {
+	public Server(String server_ip, int server_port) {
 		this.port = server_port;
 		this.ip = server_ip;
 		
@@ -28,19 +28,17 @@ public class Server{
 	public void initServer() throws Exception {
 		
     	// Overload the kill signal
-    	Runtime.getRuntime().addShutdownHook(new Thread() {
-    		public void run() {
-    			System.out.println("Cerrando Servidor...");
-    			try{
-	    			in.close();
-	    			out.close();
-	    			listener.close();
-    			}
-    			catch(Exception e) {
-    				// TODO
-    			}
-    		}
-    	});
+    	Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			System.out.println("Cerrando Servidor...");
+			try{
+				in.close();
+				out.close();
+				listener.close();
+			}
+			catch(Exception e) {
+				// TODO
+			}
+		}));
     	
         // Creates the socket
         listener = new ServerSocket(port, 0, InetAddress.getByName(ip));
@@ -55,14 +53,16 @@ public class Server{
                 // Accept a client connection once Server receives one.
             	clientSocket = listener.accept();
                 System.out.println(client + ": Nuevo cliente");
-                
-                // Creating in/out and output streams
-                out = new ObjectOutputStream(clientSocket.getOutputStream());
-                in = new ObjectInputStream(clientSocket.getInputStream());
-                
-                // Starts threading
-                Thread t = new ClientController(clientSocket, in, out, client); 
-                t.start();
+
+                if (clientSocket != null){
+					// Creating in/out and output streams
+					out = new ObjectOutputStream(clientSocket.getOutputStream());
+					in = new ObjectInputStream(clientSocket.getInputStream());
+
+					// Starts threading
+					Thread t = new ClientController(clientSocket, in, out, client);
+					t.start();
+				}
 
                 client++;
             }
